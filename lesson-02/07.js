a31ebab3ab4746e37501582be64720906cd98c3c
Vlad <vladtarasevich244@gmail.com>
2024-04-02 19:02:36 +0300

    add 02-07 exercise

---
+++ lesson-02/07.js
@@ -0,0 +1,24 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+
+let passportMarried2 = {
+    name: passportMarried.name,
+    surname: passportMarried.surname,
+    married: true,
+    address: {
+        ...passportMarried.address
+    }
+};
+
+
+
+
+console.log(passportMarried);
+console.log(passportMarried2);
